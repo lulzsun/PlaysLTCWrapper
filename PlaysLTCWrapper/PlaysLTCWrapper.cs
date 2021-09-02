4af45be937c9e468d418c7abeecad735efe4e4cb
Jimmy Quach <jminquach@gmail.com>
2021-09-02 13:37:49 -0700

    Convert to .NET Core & Removed SimpleTCP

--- PlaysLTCWrapper/PlaysLTCWrapper.cs
+++ PlaysLTCWrapper/PlaysLTCWrapper.cs
@@ -1,115 +1,23 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using SimpleTCP;
 
 namespace PlaysLTCWrapper {
     public class LTCProcess {
-        SimpleTcpServer server;
+        TcpListener server;
+        NetworkStream ns;
         Process ltcProcess;
         public void Connect() {
             Process currentProcess = Process.GetCurrentProcess();
             string pid = currentProcess.Id.ToString();
             int port = 9500;
-
-            server = new SimpleTcpServer().Start(port);
-
-            server.Delimiter = Convert.ToByte('\f');
-            server.DelimiterDataReceived += (sender, msg) => {
-                string jsonData = msg.MessageString;
-                JObject jsonObject = GetDataType(jsonData);
-                string type = jsonObject["type"].ToString();
-
-                switch (type) {
-                    case "LTC:handshake":
-                        GetEncoderSupportLevel();
-                        SetSavePaths("F:/Videos/Plays/", "F:/Videos/Plays/.temp/");
-                        SetGameDVRQuality(10, 30, 720);
-
-                        ConnectionHandshakeArgs connectionHandshakeArgs = new ConnectionHandshakeArgs {
-                            Version = jsonObject["data"]["version"].ToString(),
-                            IntegrityCheck = jsonObject["data"]["integrityCheck"].ToString()
-                        };
-                        OnConnectionHandshake(connectionHandshakeArgs);
-                        break;
-                    case "LTC:processCreated":
-                        ProcessCreatedArgs processCreatedArgs = new ProcessCreatedArgs {
-                            Pid = Int32.Parse(jsonObject["data"]["pid"].ToString()),
-                            ExeFile = jsonObject["data"]["exeFile"].ToString(),
-                            CmdLine = jsonObject["data"]["cmdLine"].ToString()
-                        };
-                        OnProcessCreated(processCreatedArgs);
-                        break;
-                    case "LTC:processTerminated":
-                        ProcessTerminatedArgs processTerminatedArgs = new ProcessTerminatedArgs {
-                            Pid = Int32.Parse(jsonObject["data"]["pid"].ToString())
-                        };
-                        OnProcessTerminated(processTerminatedArgs);
-                        break;
-                    case "LTC:graphicsLibLoaded":
-                        GraphicsLibLoadedArgs graphicsLibLoadedArgs = new GraphicsLibLoadedArgs {
-                            Pid = Int32.Parse(jsonObject["data"]["pid"].ToString()),
-                            ModuleName = jsonObject["data"]["moduleName"].ToString()
-                        };
-                        OnGraphicsLibLoaded(graphicsLibLoadedArgs);
-                        break;
-                    case "LTC:moduleLoaded":
-                        ModuleLoadedArgs moduleLoadedArgs = new ModuleLoadedArgs {
-                            Pid = Int32.Parse(jsonObject["data"]["pid"].ToString()),
-                            ModuleName = jsonObject["data"]["moduleName"].ToString()
-                        };
-                        OnModuleLoaded(moduleLoadedArgs);
-                        break;
-                    case "LTC:gameLoaded":
-                        GameLoadedArgs gameLoadedArgs = new GameLoadedArgs {
-                            Pid = Int32.Parse(jsonObject["data"]["pid"].ToString()),
-                            Width = Int32.Parse(jsonObject["data"]["size"]["width"].ToString()),
-                            Height = Int32.Parse(jsonObject["data"]["size"]["height"].ToString())
-                        };
-                        OnGameLoaded(gameLoadedArgs);
-                        break;
-                    case "LTC:videoCaptureReady":
-                        VideoCaptureReadyArgs videoCaptureReadyArgs = new VideoCaptureReadyArgs {
-                            Pid = Int32.Parse(jsonObject["data"]["pid"].ToString())
-                        };
-                        OnVideoCaptureReady(videoCaptureReadyArgs);
-                        break;
-                    case "LTC:recordingError":
-                        int errorCode = Int32.Parse(jsonObject["data"]["code"].ToString());
-                        Console.Write("Recording Error code: {0} ", errorCode);
-                        switch (errorCode) {
-                            case 11:
-                                Console.WriteLine("- Issue with video directories");
-                                break;
-                            case 16:
-                                Console.WriteLine("- Issue with disk space");
-                                break;
-                            default:
-                                Console.WriteLine();
-                                break;
-                        }
-                        break;
-                    case "LTC:gameScreenSizeChanged":
-                        Console.WriteLine("Game screen size changed, {0}x{1}", jsonObject["data"]["width"], jsonObject["data"]["height"]);
-                        break;
-                    case "LTC:saveStarted":
-                        Console.WriteLine("Started saving recording to file, {0}", jsonObject["data"]["filename"]);
-                        break;
-                    case "LTC:saveFinished":
-                        Console.WriteLine("Finished saving recording to file, {0}, {1}x{2}, {3}, {4}", 
-                                            jsonObject["data"]["fileName"], 
-                                            jsonObject["data"]["width"], 
-                                            jsonObject["data"]["height"], 
-                                            jsonObject["data"]["duration"], 
-                                            jsonObject["data"]["recMode"]);
-                        break;
-                    default:
-                        Console.WriteLine("WARNING: WAS SENT AN EVENT THAT DOES NOT MATCH CASE: {0}", jsonObject);
-                        break;
-                }
-            };
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
 
             ltcProcess = new Process {
                 StartInfo = new ProcessStartInfo {
@@ -121,14 +29,137 @@ namespace PlaysLTCWrapper {
                 }
             };
 
-            ltcProcess.Start();
-            while (!ltcProcess.StandardOutput.EndOfStream) {
-                string line = ltcProcess.StandardOutput.ReadLine();
-                Console.WriteLine(line);
-            }
+            ltcProcess.OutputDataReceived += new DataReceivedEventHandler((s, e) => {
+                Console.WriteLine(e.Data);
+            });
 
-            Console.WriteLine("PlaysTVComm.exe has ended!!!");
-            ltcProcess.Close();
+            ltcProcess.Start();
+            while (true) {
+                TcpClient client = server.AcceptTcpClient();
+                ns = client.GetStream();
+
+                while (client.Connected) {
+                    int streamByte = ns.ReadByte();
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    while (streamByte != 12)
+                    {
+                        stringBuilder.Append((char)streamByte);
+                        streamByte = ns.ReadByte();
+                    }
+
+                    string msg = stringBuilder.ToString();
+                    Console.WriteLine("RECEIVE: " + msg);
+
+                    string jsonData = msg;
+                    JObject jsonObject = GetDataType(jsonData);
+                    string type = jsonObject["type"].ToString();
+
+                    switch (type)
+                    {
+                        case "LTC:handshake":
+                            GetEncoderSupportLevel();
+                            SetSavePaths("G:/Videos/Plays/", "G:/Videos/Plays/.temp/");
+                            SetGameDVRQuality(10, 30, 720);
+
+                            ConnectionHandshakeArgs connectionHandshakeArgs = new ConnectionHandshakeArgs
+                            {
+                                Version = jsonObject["data"]["version"].ToString(),
+                                IntegrityCheck = jsonObject["data"]["integrityCheck"].ToString()
+                            };
+                            OnConnectionHandshake(connectionHandshakeArgs);
+                            break;
+                        case "LTC:processCreated":
+                            ProcessCreatedArgs processCreatedArgs = new ProcessCreatedArgs
+                            {
+                                Pid = Int32.Parse(jsonObject["data"]["pid"].ToString()),
+                                ExeFile = jsonObject["data"]["exeFile"].ToString(),
+                                CmdLine = jsonObject["data"]["cmdLine"].ToString()
+                            };
+                            OnProcessCreated(processCreatedArgs);
+                            break;
+                        case "LTC:processTerminated":
+                            ProcessTerminatedArgs processTerminatedArgs = new ProcessTerminatedArgs
+                            {
+                                Pid = Int32.Parse(jsonObject["data"]["pid"].ToString())
+                            };
+                            OnProcessTerminated(processTerminatedArgs);
+                            break;
+                        case "LTC:graphicsLibLoaded":
+                            GraphicsLibLoadedArgs graphicsLibLoadedArgs = new GraphicsLibLoadedArgs
+                            {
+                                Pid = Int32.Parse(jsonObject["data"]["pid"].ToString()),
+                                ModuleName = jsonObject["data"]["moduleName"].ToString()
+                            };
+                            OnGraphicsLibLoaded(graphicsLibLoadedArgs);
+                            break;
+                        case "LTC:moduleLoaded":
+                            ModuleLoadedArgs moduleLoadedArgs = new ModuleLoadedArgs
+                            {
+                                Pid = Int32.Parse(jsonObject["data"]["pid"].ToString()),
+                                ModuleName = jsonObject["data"]["moduleName"].ToString()
+                            };
+                            OnModuleLoaded(moduleLoadedArgs);
+                            break;
+                        case "LTC:gameLoaded":
+                            GameLoadedArgs gameLoadedArgs = new GameLoadedArgs
+                            {
+                                Pid = Int32.Parse(jsonObject["data"]["pid"].ToString()),
+                                Width = Int32.Parse(jsonObject["data"]["size"]["width"].ToString()),
+                                Height = Int32.Parse(jsonObject["data"]["size"]["height"].ToString())
+                            };
+                            OnGameLoaded(gameLoadedArgs);
+                            break;
+                        case "LTC:videoCaptureReady":
+                            VideoCaptureReadyArgs videoCaptureReadyArgs = new VideoCaptureReadyArgs
+                            {
+                                Pid = Int32.Parse(jsonObject["data"]["pid"].ToString())
+                            };
+                            OnVideoCaptureReady(videoCaptureReadyArgs);
+                            break;
+                        case "LTC:recordingError":
+                            int errorCode = Int32.Parse(jsonObject["data"]["code"].ToString());
+                            Console.Write("Recording Error code: {0} ", errorCode);
+                            switch (errorCode)
+                            {
+                                case 11:
+                                    Console.WriteLine("- Issue with video directory");
+                                    break;
+                                case 12:
+                                    Console.WriteLine("- Issue with temp directory");
+                                    break;
+                                case 16:
+                                    Console.WriteLine("- Issue with disk space");
+                                    break;
+                                default:
+                                    Console.WriteLine();
+                                    break;
+                            }
+                            break;
+                        case "LTC:gameScreenSizeChanged":
+                            Console.WriteLine("Game screen size changed, {0}x{1}", jsonObject["data"]["width"], jsonObject["data"]["height"]);
+                            break;
+                        case "LTC:saveStarted":
+                            Console.WriteLine("Started saving recording to file, {0}", jsonObject["data"]["filename"]);
+                            break;
+                        case "LTC:saveFinished":
+                            Console.WriteLine("Finished saving recording to file, {0}, {1}x{2}, {3}, {4}",
+                                                jsonObject["data"]["fileName"],
+                                                jsonObject["data"]["width"],
+                                                jsonObject["data"]["height"],
+                                                jsonObject["data"]["duration"],
+                                                jsonObject["data"]["recMode"]);
+                            break;
+                        default:
+                            Console.WriteLine("WARNING: WAS SENT AN EVENT THAT DOES NOT MATCH CASE: {0}", jsonObject);
+                            break;
+                    }
+                }
+
+                client.Close();
+                ltcProcess.Close();
+                server.Stop();
+            }
         }
 
         public void GetEncoderSupportLevel() {
@@ -213,9 +244,12 @@ namespace PlaysLTCWrapper {
         public void Emit(string type, string data = "{}") {
             data = data.Replace("'", "\"");
             string json = "{ \"type\": \"" + type + "\", \"data\": " + data + " }\f";
-            //Console.WriteLine(json);
-            if(server != null && server.IsStarted)
-                server.Broadcast(json);
+
+            if(ns != null && server != null) {
+                byte[] jsonBytes = Encoding.Default.GetBytes(json);
+                ns.Write(jsonBytes, 0, jsonBytes.Length);     //sending the message
+                Console.WriteLine("SEND: " + json);
+            }
         }
 
         public JObject GetDataType(string jsonData) {
